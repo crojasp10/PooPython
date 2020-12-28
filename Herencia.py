9822625789162084874f62f2e52aadf087a5fa1c
CarlosRojas <crojasw100@gmail.com>
2020-12-28 16:47:27 -0500

    Herencia en python

---
+++ Herencia.py
@@ -0,0 +1,29 @@
+class vehiculo():
+
+    def __init__(self,marca,modelo):
+
+        self.marca=marca
+        self.modelo=modelo
+        self.enmarcha=False
+        self.acelera=False
+        self.frena=False
+    
+    def arrancar(self):
+        self.enmarcha=True
+
+    def acelerar(self):
+        self.acelera=True
+    
+    def frenar(self):
+        self.frena=True
+
+    def estado(self):
+        print("Marca:", self.marca, "\nModelo: ", self.modelo, "\nEn marcha:",
+            self.enmarcha,"\nAcelerando:",self.acelera, "\nFrenando:",self.frena)
+
+class Moto(vehiculo):
+    pass
+
+miMoto=Moto("Honda","CBR")
+
+print(miMoto.estado())
